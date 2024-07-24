a007a360ac759ba7e486ac8e68bf8382474b2b27
matthew-kal <matthewkalender@gmail.com>
2024-07-25 00:07:44 +0300

    backend config

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,6 @@ from datetime import datetime, timedelta
 
 app = FastAPI()
 
-# Enable CORS
 origins = [
     "http://localhost",
     "http://localhost:3000",
@@ -24,7 +23,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Define the LSTM model class
 class LSTM(nn.Module):
     def __init__(self, hs, drop=0.3):
         super().__init__()
@@ -43,9 +41,8 @@ class LSTM(nn.Module):
         out = self.fc2(out)
         return out
 
-# Load the model and scaler
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-hs = 100  # Ensure this matches the value used during training
+hs = 100  
 model = LSTM(hs)
 model.load_state_dict(torch.load('lstm_model.pth', map_location=device))
 model.to(device)
@@ -67,25 +64,20 @@ def prep_df(df, steps):
 def predict(request: StockRequest):
     ticker = request.ticker
     end_date = datetime.now().date() - timedelta(days=1)
-    start_date = end_date - timedelta(days=10)  # Fetch a larger window of data initially
+    start_date = end_date - timedelta(days=10)  
     df = yf.download(ticker, start=start_date, end=end_date)[['Close']]
     initial_length = len(df)
 
-    # If initial data length is less than the lookback period + 1, fetch more data
     if initial_length < 6:
-        start_date = end_date - timedelta(days=20)  # Further adjust if necessary
+        start_date = end_date - timedelta(days=20)  
         df = yf.download(ticker, start=start_date, end=end_date)[['Close']]
+    
 
-    # Check the received data
     if df.empty:
         return {"error": "No data received from Yahoo Finance"}
-    
-    print(f"Initial DataFrame length: {len(df)}")
 
-    lb = 5  # Lookback period
+    lb = 5  
     df_prepared = prep_df(df, lb)
-
-    print(f"Prepared DataFrame length: {len(df_prepared)}")
 
     if df_prepared.empty:
         return {"error": "Not enough data after processing to make a prediction"}
@@ -97,15 +89,12 @@ def predict(request: StockRequest):
     X = X.reshape((-1, lb, 1))
     X = torch.tensor(X).float().to(device)
 
-    # Capture the last 5 lookback values
     results = df['Close'].values[-lb:].tolist()
 
-    # Make one prediction
     with torch.no_grad():
         pred = model(X[-1].unsqueeze(0)).cpu().numpy().flatten()
         results.append(pred[0])
 
-    # Inverse transform the prediction to get actual values
     dummy = np.zeros((1, data.shape[1]))
     dummy[0, 0] = results[-1]
     dummy = scaler.inverse_transform(dummy)
